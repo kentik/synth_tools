9e313f045d307e4eadf474a4c3a5960c346783fc
martin <martin@kentik.com>
2021-12-09 00:57:01 -0800

    Improve reporting of API failures while fetching test results

--- synth_tools/commands/tests.py
+++ synth_tools/commands/tests.py
@@ -231,7 +231,11 @@ def get_test_health(
     """
     api = get_api(ctx)
     t = _get_test_by_id(api.syn, test_id)
-    health = api.syn.results(t, periods=periods)
+    try:
+        health = api.syn.results(t, periods=periods)
+    except Exception as exc:
+        fail(f"Failed to get results ({exc}")
+        health = None  # to make linters happy (this line is never reached)
 
     if not health:
         fail(f"Test '{test_id}' did not produce any health data")
