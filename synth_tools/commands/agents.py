8625d51841884a97ce8a12fff781423d79b2453d
martin <martin@kentik.com>
2022-01-14 13:40:36 -0800

    Make isort happy.

--- synth_tools/commands/agents.py
+++ synth_tools/commands/agents.py
@@ -4,15 +4,7 @@ import typer
 
 from kentik_synth_client import KentikAPIRequestError, KentikSynthClient
 from synth_tools.matchers import all_matcher_from_rules
-from synth_tools.utils import (
-    agent_to_dict,
-    api_request,
-    fail,
-    get_api,
-    print_agent,
-    print_agents_brief,
-    sort_id,
-)
+from synth_tools.utils import agent_to_dict, api_request, fail, get_api, print_agent, print_agents_brief, sort_id
 
 agents_app = typer.Typer()
 
