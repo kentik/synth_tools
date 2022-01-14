8320258afc66e87454ad3c794869ef65841701a7
Martin Machacek <martin+github@kentik.com>
2022-01-14 15:44:00 -0800

    Make agent activation and deactivation work again (#44)
    * Make agent activation and deactivation work again

--- synth_tools/commands/agents.py
+++ synth_tools/commands/agents.py
@@ -4,16 +4,7 @@ import typer
 
 from kentik_synth_client import KentikAPIRequestError, KentikSynthClient
 from synth_tools.matchers import all_matcher_from_rules
-from synth_tools.utils import (
-    agent_to_dict,
-    api_request,
-    fail,
-    get_api,
-    print_agent,
-    print_agents_brief,
-    snake_to_camel,
-    sort_id,
-)
+from synth_tools.utils import agent_to_dict, api_request, fail, get_api, print_agent, print_agents_brief, sort_id
 
 agents_app = typer.Typer()
 
@@ -115,8 +106,8 @@ def activate_agent(
             typer.echo(f"id: {i} agent not pending (status: {a['status']})")
             continue
         a["status"] = "AGENT_STATUS_OK"
-        del a[snake_to_camel("site_name")]
-        a = api_request(api.syn.update_agent, "AgentUpdate", i, a)
+        del a["name"]
+        a = api_request(api.syn.patch_agent, "AgentPatch", i, a, "agent.status")
         if a["status"] != "AGENT_STATUS_OK":
             typer.echo(f"id: {i} FAILED to activate (status: {a['status']}")
         else:
@@ -138,8 +129,8 @@ def deactivate_agent(
             typer.echo(f"id: {i} agent is not active (status: {a['status']})")
             continue
         a["status"] = "AGENT_STATUS_WAIT"
-        del a[snake_to_camel("site_name")]
-        a = api_request(api.syn.patch_agent, "AgentPatch", i, a)
+        del a["name"]
+        a = api_request(api.syn.patch_agent, "AgentPatch", i, a, "agent.status")
         if a["status"] != "AGENT_STATUS_WAIT":
             typer.echo(f"id: {i} FAILED to deactivate (status: {a['status']}")
         else:
